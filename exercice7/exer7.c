d0566b67a0b5bbea81388d41f966ad95599eecb7
Azer Hasnaoui <62300519+Rednaks69@users.noreply.github.com>
2024-01-28 17:46:03 +0100

    Update exer7.c

--- exercice7/exer7.c
+++ exercice7/exer7.c
@@ -4,20 +4,12 @@
 int main(int argc, char **argv)
 {
     char str[5];
-    char *ptr = str;
-    int count = 0;
 
     printf("entrer une chaine de caractaire\t");
     fgets(str, 10, stdin);
-    str[strlen(str) - 1] = ' ';
+ 
 
-    while (*(ptr) != ' ')
-    {
-        *ptr++;
-        count++;
-    }
-
-    printf("la chaine que vous avez enter est: %s\t,et sa longeur est: %d \n", str, count);
+    printf("la chaine que vous avez enter est: %s\t,et sa longeur est: %d \n", str, strlen(str));
 
     return 0;
 }
